44b9a9a31ffcb729283b661f4a77f84f7de028c2
Ryan McCrory <ryanmccrory83@gmail.com>
2019-02-21 19:50:51 -0600

    removed un-needed comments and code from app file

--- app.js
+++ app.js
@@ -25,25 +25,5 @@ app.use('/mrandmrs', mrandmrs)
 app.use('/reception', reception)
 app.use('/tripphotos', tripphotos)
 
-// catch 404 and forward to error handler
-//app.use((req, res, next) => {
-// const err = new Error('Not Found')
-//  err.status = 404
-// next(err)
-//})
-
-// error handler
-//app.use((err, req, res, next) => {
-//  res.status(err.status || 500)
-//  res.json({
-//    message: err.message,
-//    error: req.app.get('env') === 'development' ? err.stack : {}
-//  })
-//})
-
-//app
-//  .listen(port)
-//  .on('error', console.error.bind(console))
-//  .on('listening', console.log.bind(console, 'Listening on ' + port))
 
 module.exports = app
