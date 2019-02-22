3e5299c889bb147de9c0cc48553d6ed58cb7f868
Ryan McCrory <ryanmccrory83@gmail.com>
2019-02-21 21:17:23 -0600

    addded heroku port variable setting

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = require('./app')
 const port = parseInt(process.env.PORT || 3000)
 
 app
-    .listen(port)
+    .listen(port, function () {
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    })
     .on('error', () => {
         console.log('Error, error, we have and error')
     })
